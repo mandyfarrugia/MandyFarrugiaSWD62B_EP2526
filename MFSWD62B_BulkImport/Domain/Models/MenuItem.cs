8408293f6be2c3fc37bcfa37830de8522c606070
mandyfarrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-11-15 09:05:12 +0100

    Added ExternalId attribute which stores the id of the each JSON object. Therefore, the instance will have an ID uniquely generated by Entity Framework Core, as well as an external ID uniquely identifying each JSON object. Furthermore, Status has been renamed to IsApproved for the sake of readability.

--- MFSWD62B_BulkImport/Domain/Models/MenuItem.cs
+++ MFSWD62B_BulkImport/Domain/Models/MenuItem.cs
@@ -6,12 +6,18 @@ using System.Threading.Tasks;
 
 namespace Domain.Models
 {
+    /// <summary>
+    /// A model class representing a menu item which can be related to one specific restaurant by means of the RestaurantId attribute.
+    /// Each instance of thereof requires approval of the owner of the restaurant. Once approved, the menu items will be visible to the public catalogue if and only if they approved by the restaurant owner.
+    /// </summary>
     public class MenuItem
     {
         public Guid Id { get; set; }
+        public string ExternalId { get; set; }
         public string Title { get; set; }
         public double Price { get; set; }
+        public string Currency { get; set; }
         public int RestaurantId { get; set; }
-        public string Status { get; set; }
+        public bool IsApproved { get; set; }
     }
 }
--- MFSWD62B_BulkImport/Domain/Models/Restaurant.cs
+++ MFSWD62B_BulkImport/Domain/Models/Restaurant.cs
@@ -9,8 +9,12 @@ namespace Domain.Models
     public class Restaurant
     {
         public int Id { get; set; }
+        public string ExternalId { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public string OwnerEmailAddress { get; set; }
-        public string Status { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public bool IsApproved { get; set; }
     }
 }
