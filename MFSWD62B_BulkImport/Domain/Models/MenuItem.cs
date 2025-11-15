95abb010c1c91e8c8d70a1564c43d94e532c39fa
mandyfarrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-11-15 08:26:56 +0100

    Minimalist design of the model classes. The next course of action will be to apply database related annotations to the properties such as to define a primary key constraint, as well as foreign key constraints.

---
+++ MFSWD62B_BulkImport/Domain/Models/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class MenuItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public int RestaurantId { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ MFSWD62B_BulkImport/Domain/Models/Restaurant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string OwnerEmailAddress { get; set; }
+        public string Status { get; set; }
+    }
+}
